b823f7d5d479e424e5d8d1a51e58b94d4781a300
Krerkkiat Chusap <krerkkiat@users.noreply.github.com>
2022-05-21 09:02:33 -0400

    feat: implement the file reading

--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <fstream>
 #include <sstream>
 #include <stdexcept>
 #include <vector>
 #include <string>
+#include <cmath>
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+#include <Windows.h>
+#elif __linux__
+#include <unistd.h>
+#endif
 
 #include <curlpp/cURLpp.hpp>
 #include <curlpp/Easy.hpp>
@@ -11,10 +18,13 @@
 
 #include <cxxopts.hpp>
 #include <fmt/core.h>
+#include <fmt/ostream.h>
 #include <nlohmann/json.hpp>
 #include <dotenv.h>
 
 using json = nlohmann::json;
+
+const int WAIT_DURATION = 5; // [s]
 
 namespace ghapi {
     struct payload {
@@ -31,6 +41,47 @@ namespace ghapi {
         j.at("email").get_to(p.email);
         j.at("role").get_to(p.email);
         j.at("team_ids").get_to(p.team_ids);
+    }
+}
+
+void send_invitation(const std::string& token, const std::string &org_name, int team_id, const std::string& email_address) {
+    // Construct the payload.
+    ghapi::payload p {email_address, "direct_member", std::vector<int>{team_id} };
+    json payload_j = p;
+    std::string body = payload_j.dump();
+    std::cout << payload_j.dump();
+
+    curlpp::Easy request;
+
+    std::list<std::string> header;
+    header.push_back("User-Agent: OrgBulkInvite");
+    header.push_back(fmt::format("Authorization: token {}", token));
+    header.push_back("Content-Type: application/json");
+    header.push_back("Accept: application/vnd.github.v3+json");
+
+    using namespace curlpp::Options;
+    request.setOpt(new CustomRequest{"POST"});
+    request.setOpt(new Url(fmt::format("https://api.github.com/orgs/{:s}/invitations", org_name)));
+    request.setOpt(new HttpHeader(header));
+    request.setOpt(new PostFields(body));
+    request.setOpt(new PostFieldSize(body.length()));
+    request.setOpt(new Verbose(true));
+
+    std::ostringstream response;
+    request.setOpt(new WriteStream(&response));
+
+    bool is_sent = false;
+    unsigned int attempt_count = 0;
+    while (!is_sent) {
+        try {
+            request.perform();
+            is_sent = true;
+            std::cout << response.str() << std::endl;
+        } catch (curlpp::RuntimeError &e) {
+            std::cerr << "runtime error, waiting before try again\n";
+            attempt_count += 1;
+            sleep(pow(2, attempt_count));
+        }
     }
 }
 
@@ -66,46 +117,36 @@ int main(int argc, char *argv[]) {
     }
 
     // Read in the input file.
-    // TODO(kchusap): sanitize the email addresses. skip the email with invalid domain.
-    // TODO(kchusap): implement back-off so the program can recover from transient-fault from the network.
-    // TODO(kchusap): give enough time between request, so that we are not rate limitted.
-
-    // Construct the payload.
-    ghapi::payload p {"bobcat@ohio.edu", "direct_member", std::vector<int>{team_id} };
-    json payload_j = p;
-    std::string body = payload_j.dump();
-    std::cout << payload_j.dump();
+    std::ifstream email_file;
+    email_file.open(input_filename.c_str());
+    if (!email_file.is_open()) {
+        fmt::print(std::cerr, "failed to open file '{}''\n", input_filename);
+        return -1;
+    }
 
     try {
         curlpp::Cleanup cleaner;
-        curlpp::Easy request;
+        std::string line;
+        while (std::getline(email_file, line)) {
+            auto pos = line.rfind("@ohio.edu");
+            if (pos == std::string::npos) {
+                fmt::print("'{}' is not a valid OHIO email address. skip sending an invitation.\n", line);
+                sleep(WAIT_DURATION);
+                continue;
+            }
+            fmt::print("sending an invitation to '{}'\n", line);
+            // send_invitation(token, org_name, team_id, line);
 
-        std::list<std::string> header;
-        header.push_back("User-Agent: OrgBulkInvite");
-        header.push_back(fmt::format("Authorization: token {}", token));
-        header.push_back("Content-Type: application/json");
-        header.push_back("Accept: application/vnd.github.v3+json");
-
-        using namespace curlpp::Options;
-        request.setOpt(new CustomRequest{"POST"});
-        request.setOpt(new Url(fmt::format("https://api.github.com/orgs/{:s}/invitations", org_name)));
-        request.setOpt(new HttpHeader(header));
-        request.setOpt(new PostFields(body));
-        request.setOpt(new PostFieldSize(body.length()));
-        request.setOpt(new Verbose(true));
-
-        std::ostringstream response;
-        request.setOpt(new WriteStream(&response));
-
-        request.perform();
-
-        std::cout << response.str() << std::endl;
+            sleep(WAIT_DURATION);
+        }
 
     } catch (curlpp::LogicError &e) {
         std::cout << e.what() << std::endl;
     } catch (curlpp::RuntimeError &e) {
         std::cout << e.what() << std::endl;
     }
+    email_file.close();
+
 
     return 0;
 }
